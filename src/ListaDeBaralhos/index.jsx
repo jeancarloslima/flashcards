7f2a878f4ec71343b926e5272d64e8a5fa517c91
Jean Lima <jcoliveiralima19@gmail.com>
2026-02-20 14:04:25 -0300

    Alterando o design da lista de baralhos

--- src/ListaDeBaralhos/index.jsx
+++ src/ListaDeBaralhos/index.jsx
@@ -4,7 +4,7 @@ import './lista-de-baralhos.css';
 export default function ListaDeBaralhos() {
     return (
         <ul id="lista-de-baralhos">
-            <li className="item-baralho"><TbCards /> <span>INGLÊS</span></li>
+            <li className="item-baralho baralho-selecionado"><TbCards /> <span>INGLÊS</span></li>
             <li className="item-baralho"><TbCards /> <span>Programação</span></li>
             <li className="item-baralho"><TbCards /> <span>INGLÊS</span></li>
             <li className="item-baralho"><TbCards /> <span>INGLÊS</span></li>
