038ed98d053964d1daed0a2d9550e99c2b3dc2e8
Jean Lima <jcoliveiralima19@gmail.com>
2026-02-20 13:49:00 -0300

    Implementando design da lista de baralhos

--- src/ListaDeBaralhos/index.jsx
+++ src/ListaDeBaralhos/index.jsx
@@ -1,9 +1,16 @@
+import { TbCards } from 'react-icons/tb';
 import './lista-de-baralhos.css';
 
 export default function ListaDeBaralhos() {
     return (
-        <ul id="lista-de-baralhos">
-            <li className="item-baralho">INGLÊS</li>
-        </ul>
+        <div id='lista-de-baralhos-container'>
+            <ul id="lista-de-baralhos">
+                <li className="item-baralho"><TbCards /> <span>INGLÊS</span></li>
+                <li className="item-baralho"><TbCards /> <span>Programação</span></li>
+                <li className="item-baralho"><TbCards /> <span>INGLÊS</span></li>
+                <li className="item-baralho"><TbCards /> <span>INGLÊS</span></li>
+            </ul>
+            <button id="btn-adicionar-baralho">+</button>
+        </div>
     )
 }
