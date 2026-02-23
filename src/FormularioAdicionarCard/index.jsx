df5387a2ca163b2a43e2decf9ea966f1eae4ffc1
Jean Lima <jcoliveiralima19@gmail.com>
2026-02-23 14:01:29 -0300

    Adicionando modal overlay para os formulários

--- src/FormularioAdicionarCard/index.jsx
+++ src/FormularioAdicionarCard/index.jsx
@@ -2,10 +2,12 @@ import './formulario-adicionar-card.css';
 
 export default function FormularioAdicionarCard( {fecharFormularioCard} ) {
     return(
-        <form id="formulario-adicionar-card">
-            <input type="text" id="pergunta-card" placeholder='Work' />
-            <input type="text" id="resposta-card" placeholder='Trabalhar' />
-            <input type="submit" id='btn-salvar-card' value="ADICIONAR" onClick={fecharFormularioCard} />
-        </form>
+        <div className="modal-overlay" onClick={fecharFormularioCard} >
+            <form id="formulario-adicionar-card" onClick={(e) => e.stopPropagation()}>
+                <input type="text" id="pergunta-card" placeholder='Work' />
+                <input type="text" id="resposta-card" placeholder='Trabalhar' />
+                <input type="submit" id='btn-salvar-card' value="ADICIONAR" onClick={fecharFormularioCard} />
+            </form>
+        </div>
     )
 }
--- src/FormularioAdicionarDeck/index.jsx
+++ src/FormularioAdicionarDeck/index.jsx
@@ -2,9 +2,11 @@ import './formulario-adicionar-deck.css';
 
 export default function FormularioAdicionarDeck( {fecharFormularioDeck} ) {
     return(
-        <form id="formulario-adicionar-deck">
-            <input type="text" id="nome-deck" placeholder='Inglês' />
-            <input type="submit" id='btn-salvar-deck' value="ADICIONAR" onClick={fecharFormularioDeck} />
-        </form>
+        <div className="modal-overlay" onClick={fecharFormularioDeck}>
+            <form id="formulario-adicionar-deck" onClick={(e) => e.stopPropagation()}>
+                <input type="text" id="nome-deck" placeholder='Inglês' />
+                <input type="submit" id='btn-salvar-deck' value="ADICIONAR" onClick={fecharFormularioDeck}/>
+            </form>
+        </div>
     )
 }
