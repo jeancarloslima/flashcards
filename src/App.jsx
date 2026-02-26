a87a7caa9d72a997b27ec77b7c2f884cdb565550
Jean Lima <jcoliveiralima19@gmail.com>
2026-02-26 16:45:17 -0300

    Recomeçando o projeto do zero, porque estava um caos

--- src/App.jsx
+++ src/App.jsx
@@ -1,90 +1,9 @@
-import { useState } from "react";
-import { AnimatePresence } from "framer-motion";
 import "./App.css";
-import FlashCard from "./FlashCard";
-import BotoesCertoErrado from "./BotoesCertoErrado";
-import ListaDeBaralhos from "./ListaDeBaralhos";
-import BotaoAdicionarCard from "./BotaoAdicionarCard";
-import FormularioAdicionarCard from "./FormularioAdicionarCard";
-import FormularioAdicionarDeck from "./FormularioAdicionarDeck";
 
 function App() {
-  const [isFormDeckVisible, setIsFormDeckVisible] = useState(false);
-  const [isFormCardVisible, setIsFormCardVisible] = useState(false);
-  const [acaoCard, setAcaoCard] = useState(null);
-  const [decks, setDecks] = useState([]);
-  const [flashcards, setFlashcards] = useState([]);
-
-  let acertos = 0;
-  let erros = 0;
-
-  const handleAcaoCard = (direcao) => {
-    setAcaoCard(direcao);
-
-    setFlashcards(lista => lista.slice(1));
-
-    setTimeout(() => {
-      setAcaoCard(null);
-    }, 500);
-    
-    if (direcao === "certo") {
-      acertos++;
-    } else {
-      erros++;
-    }
-  };
-
-  function adicionarDeck(novoDeck) {
-    setDecks([...decks, novoDeck]);
-  }
-
-  function adicionarFlashcard(novoFlashcard) {
-    setFlashcards([...flashcards, novoFlashcard]);
-  }
-
   return (
-    <>
-      <header>
-        <ListaDeBaralhos
-          abrirFormularioDeck={() => setIsFormDeckVisible(true)}
-          decks={decks}
-        />
-      </header>
 
-      <main>
-        <AnimatePresence>
-          {isFormCardVisible && (
-            <FormularioAdicionarCard
-              fecharFormularioCard={() => setIsFormCardVisible(false)}
-              adicionaFlashcardNaLista={adicionarFlashcard}
-            />
-          )}
-          {isFormDeckVisible && (
-            <FormularioAdicionarDeck
-              fecharFormularioDeck={() => setIsFormDeckVisible(false)}
-              adicionarDeckNaLista={adicionarDeck}
-            />
-          )}
-        </AnimatePresence>
-
-        {flashcards.length > 0 && (
-          <FlashCard pergunta={flashcards[0].pergunta} resposta={flashcards[0].resposta} acaoCard={acaoCard} />
-        )}
-
-        {flashcards.length < 1 && (
-          <FlashCard acaoCard={acaoCard} />
-        )}
-
-        <BotoesCertoErrado onAction={handleAcaoCard} />
-      </main>
-
-      <footer>
-        <BotaoAdicionarCard
-          abrirFormularioCard={() => setIsFormCardVisible(true)}
-        />
-      </footer>
-    </>
-  );
+  )
 }
 
 export default App;
--- src/BotaoAdicionarCard/index.jsx
+++
@@ -1,9 +0,0 @@
-import './botao-adicionar-card.css';
-
-export default function BotaoAdicionarCard( {abrirFormularioCard} ) {
-    return (
-        <>
-            <button id="btn-adicionar-card" onClick={abrirFormularioCard}>+</button>
-        </>
-    )
-}
