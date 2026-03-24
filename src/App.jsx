f444779eae285e359d54dd354c3f98ba276df5b8
Jean Lima <jcoliveiralima19@gmail.com>
2026-03-24 11:34:19 -0300

    Adicionando botão de reiniciar

--- src/App.jsx
+++ src/App.jsx
@@ -29,14 +29,19 @@ function App() {
 
   function handleCorrecao(event) {
     if (event.target.value === "correto") {
-        setAcertos((acertosAtual) => acertosAtual + 1);
+      setAcertos((acertosAtual) => acertosAtual + 1);
     }
 
     setcartaVirada(false);
     setCartas((cartasAtuais) => cartasAtuais.slice(0, -1));
 
-    audioCardRef.current.currentTime = 0
+    audioCardRef.current.currentTime = 0;
     audioCardRef.current.play();
+  }
+
+  function handleReinicio() {
+    setCartas(cartasIniciais);
+    setAcertos(0);
   }
 
   return (
@@ -44,21 +49,43 @@ function App() {
       <main>
         <div className="main-container">
           <ul className="lista-cartas">
-            <h2>Você acertou {acertos} de {totalPerguntas} ({acertos / totalPerguntas * 100}%)</h2>
-            {cartas.length > 0 && cartas.map((carta) => (
-              <Carta
-                noCLique={handleViraCarta}
-                key={carta.id}
-                texto={!cartaVirada ? carta.frente : carta.verso}
-              />
-            ))}
+            <h2>
+              Você acertou {acertos} de {totalPerguntas} (
+              {(acertos / totalPerguntas) * 100}%)
+            </h2>
+            {cartas.length > 0 &&
+              cartas.map((carta) => (
+                <Carta
+                  noCLique={handleViraCarta}
+                  key={carta.id}
+                  texto={!cartaVirada ? carta.frente : carta.verso}
+                />
+              ))}
           </ul>
 
           <div className="container-botoes">
-            <button value="correto" onClick={handleCorrecao} className="btn-correcao btn-correto">ACERTEI</button>
-            <button value="incorreto" onClick={handleCorrecao} className="btn-correcao btn-incorreto">ERREI</button>
+            {cartas.length > 0 && (
+              <>
+                <button
+                  value="correto"
+                  onClick={handleCorrecao}
+                  className="btn-correcao btn-correto"
+                >
+                  ACERTEI
+                </button>
+                <button
+                  value="incorreto"
+                  onClick={handleCorrecao}
+                  className="btn-correcao btn-incorreto"
+                >
+                  ERREI
+                </button>
+              </>
+            )}
+            {cartas.length < 1 && (
+              <button className="btn-correcao btn-reinicia" onClick={handleReinicio}>Recomeçar</button>
+            )}
           </div>
-
         </div>
       </main>
     </div>
